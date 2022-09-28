250cd32ee9325b26525882f62269eaaa8685381b
Jxstxs <juleb0202@gmail.com>
2022-09-28 16:52:48 +0200

    feat: added wiki creation with git or non git type

--- lua/jaw/wiki.lua
+++ lua/jaw/wiki.lua
@@ -9,54 +9,56 @@ local Job = require "plenary.job"
 M.new = function()
     local on_confirm = function(input)
         if input ~= nil then
-            local cgr = config.create_git_repo
-            local cmd, args = nil, nil
 
-            if cgr == e.PROMPT then
-                -- local choice = nil
-                -- -- get input
-                --
-                -- local cc = utils.checkChoice("wiki-new", choice)
-                -- if cc == e.CHOICE_CHECK_PRO then
-                --     cgr = e.ALWAYS
-                -- elseif cc == e.CHOICE_CHECK_CON then
-                --     cgr = e.ALWAYS
-                -- elseif cc == e.CHOICE_CHECK_NONE then
-                --     vim.api.nvim_notify(config.system["wiki-new"].output.no_input, 0, {})
-                --     return
-                -- end
-                -- vim.api.nvim_notify("[jaw] Aborting.. not Implemented yet", 0, {})
-                print "[jaw] Aborting.. not Implemented yet"
-                return
-            end
             -- check if it exists
             if utils.checkPath(input) ~= true then
                 local cgr = config.create_git_repo
                 local cmd, args = nil, nil
 
-            if cgr == e.ALWAYS then
-                cmd = "git"
-                args = { "init", input }
-            elseif cgr == e.NEVER then
-                cmd = "mkdir"
-                args = { "-m", "-p", input }
+                if cgr == e.PROMPT then
+                    -- local choice = nil
+                    -- -- get input
+                    --
+                    -- local cc = utils.checkChoice("wiki-new", choice)
+                    -- if cc == e.CHOICE_CHECK_PRO then
+                    --     cgr = e.ALWAYS
+                    -- elseif cc == e.CHOICE_CHECK_CON then
+                    --     cgr = e.ALWAYS
+                    -- elseif cc == e.CHOICE_CHECK_NONE then
+                    --     vim.api.nvim_notify(config.system["wiki-new"].output.no_input, 0, {})
+                    --     return
+                    -- end
+                    -- vim.api.nvim_notify("[jaw] Aborting.. not Implemented yet", 0, {})
+                    print "[jaw] Aborting.. not Implemented yet"
+                    return
+                end
+
+                if cgr == e.ALWAYS then
+                    cmd = "git"
+                    args = { "init", input }
+                elseif cgr == e.NEVER then
+                    cmd = "mkdir"
+                    args = { "-m", "-p", input }
+                end
+
+                Job:new({
+                    command = cmd,
+                    args = args,
+                    cwd = "/usr/bin",
+                    on_stderr = config.system["wiki-new"].output.error,
+                    on_stdout = config.system["wiki-new"].output.stdout,
+                }):sync()
+
+                Job:new({
+                    command = config.shell,
+                    args = { config.new_wiki_template, input },
+                    cwd = "/usr/bin",
+                    on_stderr = config.system["wiki-new"].output.error,
+                    on_stdout = config.system["wiki-new"].output.stdout,
+                }):sync()
+            else
+                print(config.system["wiki-new"].output.path_exists)
             end
-
-            Job:new({
-                command = cmd,
-                args = args,
-                cwd = "/usr/bin",
-                on_stderr = config.system["wiki-new"].output.error,
-                on_stdout = config.system["wiki-new"].output.stdout,
-            }):sync()
-
-            Job:new({
-                command = config.shell,
-                args = { config.new_wiki_template },
-                cwd = "/usr/bin",
-                on_stderr = config.system["wiki-new"].output.error,
-                on_stdout = config.system["wiki-new"].output.stdout,
-            }):sync()
         else
             vim.api.nvim_notify(config.system["wiki-new"].output.no_input, 0, {})
         end
