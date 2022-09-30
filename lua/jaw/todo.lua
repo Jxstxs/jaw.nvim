37472e1d37d5ba0e4841ef96cf8839ed2ad26b6c
Jxstxs <juleb0202@gmail.com>
2022-09-30 13:33:24 +0200

    feat: added todoInsert

--- lua/jaw/todo.lua
+++ lua/jaw/todo.lua
@@ -1,6 +1,36 @@
 local M = {}
 
-M.insert = function() end
+local config = require("jaw.config").config
+local utils  = require("jaw.utils")
+
+M.insert = function()
+    -- get the start and end position of the selection
+    local start_selection = vim.api.nvim_buf_get_mark(0, "<")[1] -- - 1
+    local end_selection = vim.api.nvim_buf_get_mark(0, ">")[1]
+
+    -- get the selected lines
+    local lines = {}
+    for i = start_selection, end_selection, 1 do
+        table.insert(lines, { i, vim.fn.getline(i, i) })
+    end
+
+    -- check if the table is empty
+    -- TODO: better non visual selection check
+    if lines[1][1] ~= 0 and lines[1][2] ~= {} then
+        for _, value in ipairs(lines) do
+            if not utils.checkTodoLine(value[2][1]) then
+                vim.fn.setline(value[1], string.format(config.system["todo-insert"].template, value[2][1]))
+            end
+        end
+        -- clear the marks
+        vim.api.nvim_buf_del_mark(0, "<")
+        vim.api.nvim_buf_del_mark(0, ">")
+    else
+        -- gets the current line and sets it
+        local current_line = vim.api.nvim_get_current_line()
+        vim.api.nvim_set_current_line(string.format(config.system["todo-insert"].template, current_line))
+    end
+end
 
 M.toggle = function() end
 
