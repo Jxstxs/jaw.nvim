1608b710631d97a50c6dce654cca91347c362932
Jxstxs <juleb0202@gmail.com>
2022-10-01 17:01:03 +0200

    imp: summary due to multiple use

--- lua/jaw/utils.lua
+++ lua/jaw/utils.lua
@@ -40,4 +40,18 @@ M.checkTodoLine = function(line)
     return true
 end
 
+M.getVisualSelection = function ()
+    -- get the start and end position of the selection
+    local start_selection = vim.api.nvim_buf_get_mark(0, "<")[1] -- - 1
+    local end_selection = vim.api.nvim_buf_get_mark(0, ">")[1]
+
+    -- get the selected lines
+    local lines = {}
+    for i = start_selection, end_selection, 1 do
+        table.insert(lines, { i, vim.fn.getline(i, i) })
+    end
+
+    return lines
+end
+
 return M
