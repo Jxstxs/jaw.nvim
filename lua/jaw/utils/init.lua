e2ab923a5b0ef664e33a21fce0817be09ac51ecc
Jxstxs <juleb0202@gmail.com>
2022-10-03 00:57:00 +0200

    fix: restructurize and added some docs

--- lua/jaw/utils/init.lua
+++ lua/jaw/utils/init.lua
@@ -5,6 +5,10 @@ local path = require("plenary.path")
 local config = require("jaw.config").config
 local e = require("jaw.config").ENUMS
 
+-- checks if a choice is pro or contra
+-- @param choices: string: a filed of the choices table
+-- @param choice: string: the input given by the user
+-- @return int: e.CHOICE_CHECK_PRO when the choice is pro; e.CHOICE_CHECK_CON when its contra; e.CHOICE_CHECK_NONE when nether
 M.checkChoice = function(choices, choice)
     for _, value in ipairs(config.choices[choices].pro) do
         if choice == value then
@@ -40,6 +44,8 @@ M.checkEmptyLine = function(line)
     return true
 end
 
+-- gets the selected lines via the visual selection
+-- @return table: the lines inside the selection
 M.getVisualSelection = function()
     -- get the start and end position of the selection
     local start_selection = vim.api.nvim_buf_get_mark(0, "<")[1] -- - 1
@@ -52,46 +58,6 @@ M.getVisualSelection = function()
     end
 
     return lines
-end
-
--- checks the state of the todo
--- @param line: string: the string to check for the state
--- @return int: e.TODO_STATE_CHECKED when its checked; e.TODO_STATE_NON_CHECKED when not; -1 when both not found
-M.checkTodoState = function(line)
-    if string.match(line, config.system["todo"].matching.checked) then return e.TODO_STATE_CHECKED
-    elseif string.match(line, config.system["todo"].matching.non_checked) then return e.TODO_STATE_NON_CHECKED
-    else return -1
-    end
-end
-
--- checks if a line is a markdown todo (github falvour)
--- @param: string: the string to check if it contains an todo
--- @return bool: true when found; false when not
-M.checkTodoLine = function(line)
-    -- TODO: implement better ceck
-    if M.checkTodoState(line) ~= -1 then
-        return true
-    end
-    return false
-end
-
--- decides where to star the slice and which text to put into the template
--- @param state: int: the state of the current todo line
--- @return int: where to start the slice
--- @return string: which character to put inside the template
-M.parseTodoState = function(state)
-    local start
-    local state_text
-
-    if state == e.TODO_STATE_CHECKED then
-        start = 3
-        state_text = " "
-    elseif state == e.TODO_STATE_NON_CHECKED then
-        start = 4
-        state_text = config.system["todo"].checked_symbol
-    end
-
-    return start, state_text
 end
 
 -- splits a string by pattern
